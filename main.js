5285c3f0c4610756f743a0d7d1b5017251c37845
dianakang <younjung952@hufs-gsuite.kr>
2024-07-03 14:53:34 +0900

    Done 'Check' & 'Delete' button

--- main.js
+++ main.js
@@ -2,33 +2,76 @@
 // 2. When user clicks + button, then task will be added.
 // 3. When user clicks Delete button, then task will be deleted.
 //    When user clicks Check button, then task will be done and strikethrough will be applied.
-// 4. When user clicks 'In process', 'Done' on the tab, then it will be moved. 
+//    - IsCompleted: true -> false
+//    - if IsCompleted == true => strikethrough will be applied.
+//    - else => IsCompleted: false
+// 4. When user clicks 'In process', 'Done' on the tab, then it will be moved.
 //    - In process => Show in processing tasks
 //    - Done => Show done tasks
 //    - All => Show all tasks
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
-let taskList = []
-addButton.addEventListener("click",addTask)
+let taskList = [];
+addButton.addEventListener("click", addTask);
 
-function addTask(){
-    let taskContent = taskInput.value
-    taskList.push(taskContent)
-    console.log(taskList);
-    render();
+function addTask() {
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isCompleted: false,
+  };
+  taskList.push(task);
+  console.log(taskList);
+  render();
 }
 
-function render(){
-    let resultHTML = '';
-    for(let i=0;i<taskList.length; i++){
-        resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
-        <div>
-            <button>Check</button>
-            <button>Delete</button>
-        </div>
-    </div>`;
+// UI 그려주는 로직
+function render() {
+  let resultHTML = "";
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].isCompleted == true) {
+      resultHTML += `<div class="task">
+            <div class="task-done">${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+            </div>
+        </div>`;
+    } else {
+      resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+            </div>
+        </div>`;
     }
-    document.getElementById("task-board").innerHTML = resultHTML;
+  }
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isCompleted = !taskList[i].isCompleted; // 체크 표시 ON/OFF (true/false 왔다갔다)
+      break;
+    }
+  }
+  render(); // UI 업데이트 함수 호출 필수!
+  console.log(taskList);
+}
+
+function deleteTask(id){
+    for(let i=0;i<taskList.length;i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1)
+            break;
+        }
+    }
+    render(); // 값을 업데이트하면 UI도 반드시 업데이트 해주어야 한다.
+}
+
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
